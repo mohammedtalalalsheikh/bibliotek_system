9684797d73555ba2e20b3f829fa43c66b27f97df
Mohammed Alsheikh <mohammedalsheikh@MacBook-Pro-von-Mohammed.local>
2024-03-19 16:49:09 +0100

    Funktion Studenten einfuegen und alle studenten zeigen

--- bibliotek.c
+++ bibliotek.c
@@ -23,3 +23,71 @@ struct Buch
     int price;
 
 };
+
+
+const int STUDENTS_MAX = 100;
+const int BUECHER_MAX = 100;
+int student_zaehler = 0;
+
+struct Student studenten[STUDENTS_MAX];
+struct Buch buecher[BUECHER_MAX];
+
+
+void student_einfuegen(int id, char name[50], int alt){
+    studenten[student_zaehler].id = id;
+    strcpy(studenten[student_zaehler].name, name);
+    studenten[student_zaehler].alt = alt;
+    student_zaehler++;
+}
+
+void alle_studenten_zeigen()
+{
+    for (int i=0;i<student_zaehler;i++){
+        printf("Student: %d %s %d \n", studenten[i].id, studenten[i].name, studenten[i].alt);
+    }
+}
+
+
+
+int main(){
+
+
+    student_einfuegen(0, "Ahmad", 20);
+    student_einfuegen(1, "Baker", 25);
+    student_einfuegen(2, "Suzan", 19);
+
+    alle_studenten_zeigen();
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
