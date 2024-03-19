fc6e273201640202f04bbf32b8b9154f89d61a8e
Mohammed Alsheikh <mohammedalsheikh@MacBook-Pro-von-Mohammed.local>
2024-03-19 14:39:55 +0100

    structs von Student und Buch erstellen

---
+++ bibliotek.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+
+
+
+
+
+struct Student 
+{
+    int id;
+    char name[50];
+    int alt;
+};
+
+struct Buch
+{
+    int id;
+    char title[50];
+    char autor[50];
+    int price;
+
+};
