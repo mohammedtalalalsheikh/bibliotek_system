4660caccc67c5769e870eae00cffc449eb16cade
Baker Aldaood <baker.aldaood.g@gmail.com>
2024-03-21 16:27:02 +0100

    Ausleihnprozess implementieren

--- bibliotek.c
+++ bibliotek.c
@@ -32,6 +32,11 @@ struct Kaufen
 };
 
 
+struct Ausleihn
+{
+    struct Student *student;
+    struct Buch *buch;
+};
 
 const int STUDENTS_MAX = 100;
 const int BUECHER_MAX = 100;
@@ -42,6 +47,28 @@ int kauf_zaehler = 0;
 struct Student studenten[STUDENTS_MAX];
 struct Buch buecher[BUECHER_MAX];
 struct Kaufen kauf_list[BUECHER_MAX];
+struct Ausleihn ausleihn_list[BUECHER_MAX];
+
+
+void print_buch(struct Buch buch)
+{
+    printf("Buch: id: %d, Title: %s, Autor: %s, Price: %d \n", buch.id, buch.title, buch.autor, buch.price);
+}
+
+void print_student(struct Student student)
+{
+    printf("Student: id: %d, name: %s, alt: %d \n", student.id, student.name, student.alt);
+}
+
+
+void werte_von_zeigers_null_stellen()
+{
+    for (int i = 0; i<BUECHER_MAX; i++)
+    {
+        ausleihn_list[i].student = 0;
+        ausleihn_list[i].buch = 0;
+    }
+}
 
 
 int ist_buch_verkauft(struct Buch *buch)
@@ -50,6 +77,95 @@ int ist_buch_verkauft(struct Buch *buch)
         if (kauf_list[i].buch == buch)
             return 1;
     return 0;
+
+}
+
+
+int student_hat_schon_buch_ausgeliehn(struct Student *student)
+{
+    for (int i=0;i < BUECHER_MAX;i++)
+    {
+        if (ausleihn_list[i].student == student)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+
+int buch_ist_schon_ausgeliehn(struct Buch *buch)
+{
+    for (int i=0;i < BUECHER_MAX;i++)
+    {
+        
+        if (ausleihn_list[i].buch == buch)
+        {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+
+
+void zu_ausleihn(struct Student *student, struct Buch *buch)
+{
+    
+    if (student_hat_schon_buch_ausgeliehn(student))
+    {
+        printf("Leider diese Student hat schon ein Buch ausgelien. Er darf nicht mehr als ein Buch ausgelien!\n");
+        return;
+    }
+    
+    if (buch_ist_schon_ausgeliehn(buch))
+    {
+        printf("Leider dies Buch ist schon ausgelien!\n");
+        return;
+    }
+    
+
+    for (int i = 0; i < BUECHER_MAX; i++)
+    {
+        if(ausleihn_list[i].student == 0) // freie Platz
+        {
+            ausleihn_list[i].student = student;
+            ausleihn_list[i].buch = buch;
+        }
+    }
+
+}
+
+
+void zeig_was_student_ausgeliehn(struct Student *student)
+{
+    for (int i = 0; i < BUECHER_MAX ; i++)
+    {
+        if(ausleihn_list[i].student == student)
+        {
+            printf("Dies Student %s hat diesen Buch ausgelien:\n", student->name);
+            print_buch(*ausleihn_list[i].buch);
+            return;
+        }
+    }
+    printf("Dies Student %s hat kein Buch ausgelien\n", student->name);
+}
+
+void rueckgibt_was_student_ausgeliehn(struct Student *student)
+{
+
+    for (int i = 0; i < BUECHER_MAX ; i++)
+    {
+        if(ausleihn_list[i].student == student)
+        {
+            ausleihn_list[i].student = 0;
+            ausleihn_list[i].buch = 0;
+            printf("Rueckgabe Prozess wurde erfogreich durschgefuehrt\n");
+            return;
+        }
+    }
+    printf("Dies Student %s hat kein Buch ausgelien\n", student->name);
+
 
 }
 
@@ -72,15 +188,6 @@ void fuehre_kaufprozess_aus(struct Student *student, struct Buch *buch)
     kauf_zaehler++;
 }
 
-void print_buch(struct Buch buch)
-{
-    printf("Buch: id: %d, Title: %s, Autor: %s, Price: %d \n", buch.id, buch.title, buch.autor, buch.price);
-}
-
-void print_student(struct Student student)
-{
-    printf("Student: %d %s %d \n", student.id, student.name, student.alt);
-}
 
 void zeig_gekaufte_buecher_von_student(struct Student *student)
 {
@@ -135,6 +242,8 @@ void alle_buecher_zeigen()
 
 int main(){
 
+    werte_von_zeigers_null_stellen();
+
 
     student_einfuegen(0, "Ahmad", 20);
     student_einfuegen(1, "Baker", 25);
@@ -150,11 +259,18 @@ int main(){
     fuehre_kaufprozess_aus(&studenten[0], &buecher[1]);
     zeig_gekaufte_buecher_von_student(&studenten[0]);
     
-    
-    
-
     alle_studenten_zeigen();
-    alle_buecher_zeigen();
+    alle_buecher_zeigen();  
+    printf("\n\nTEST Ausleihn:\n");
+    zu_ausleihn(&studenten[0], &buecher[1]);
+    zu_ausleihn(&studenten[0], &buecher[1]);
+    
+    zeig_was_student_ausgeliehn(&studenten[0]);
+
+    rueckgibt_was_student_ausgeliehn(&studenten[0]);
+
+    zeig_was_student_ausgeliehn(&studenten[0]);
+
     return 0;
 }
 
